bb88cf170fd561f32a7f71430a4b6a37bc5ad71c
Esteve Paredes <esteve.paredes.4@gmail.com>
2024-05-18 13:50:32 -0500

    instalando react-router y configurando rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./Pages/Home/Home.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
 import "./index.css";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <RouterProvider router={router} />,
 );
